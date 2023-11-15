84363dab94110334516a6259bedd4bc61455c8e5
Samyak404 <samyak.indurkar9426@gmail.com>
2023-11-15 20:42:15 +0100

    task 2 completed

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 D = 0.1  
 mu = 0.01  
 dz_dp = -100
-N = 100  
+N = 100
 
 r_values = np.linspace(0, D/2, N)
 delta_r = r_values[1] - r_values[0]
@@ -12,16 +12,18 @@ delta_r = r_values[1] - r_values[0]
 A = np.zeros((N, N))
 b = np.zeros(N)
 
-for i in range(1, N-1):
-    A[i, i] = -2 / delta_r**2 - 1 / (r_values[i] * delta_r)
-    A[i, i-1] = 1 / delta_r**2 + 1 / (2 * r_values[i] * delta_r)
-    A[i, i+1] = 1 / delta_r**2 - 1 / (2 * r_values[i] * delta_r)
-    b[i] = dz_dp / mu
-
 A[0, 0] = 1
-A[N-1, N-1] = 1
+A[0, 1] = -1
 b[0] = 0
-b[N-1] = 0
+
+A[-1, -1] = 1
+b[-1] = 0
+
+for i in range(1, N-1):
+    A[i, i] = (-(2 / delta_r**2) + 1 / (r_values[i] * delta_r))
+    A[i, i-1] = 1 / delta_r**2 - 1 / (r_values[i] * delta_r)
+    A[i, i+1] = (1 / delta_r**2)
+    b[i] = dz_dp / mu
 
 u_z_values = np.linalg.solve(A, b)
 
